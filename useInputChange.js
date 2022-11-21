9084e612a0ca23d48fc7ca4806f1a6ee802f0d33
BohdanNeskorozhenyy <89013557+BohdanNeskorozhenyy@users.noreply.github.com>
2022-11-21 10:25:37 +0200

    Create useInputChange.js

---
+++ useInputChange.js
@@ -0,0 +1,10 @@
+export const useInputChange = () => {
+  const [input, setInput] = useState({})
+
+  const handleInputChange = (e) => setInput({
+    ...input,
+    [e.currentTarget.name]: e.currentTarget.value
+  })
+
+  return [input, handleInputChange]
+}
