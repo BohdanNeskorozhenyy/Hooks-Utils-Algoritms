36013a5b58545f4388e1144e639013cbbdd31ecd
BohdanNeskorozhenyy <89013557+BohdanNeskorozhenyy@users.noreply.github.com>
2022-11-21 10:26:34 +0200

    Update useInputChange.js

--- useInputChange.js
+++ useInputChange.js
@@ -8,3 +8,23 @@ export const useInputChange = () => {
 
   return [input, handleInputChange]
 }
+
+//=======Usage========//
+
+export const ControlledComponentWithHooks = () => {
+  const [input, handleInputChange] = useInputChange()
+
+  return (
+    <form>
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" name="password" onChange={handleInputChange} />
+      </div>
+      <input type="submit" />
+    </form>
+  )
+}
