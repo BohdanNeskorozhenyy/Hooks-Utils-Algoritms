ff5441021144f79132a326a1afcb70b3774eeb73
Bohdan <bogdaneskor@gmail.com>
2024-02-10 16:50:48 +0200

    add Dijkstra's algorithm

---
+++ src/algoritms/searching/dijkstra/README.md
@@ -0,0 +1,13 @@
+| Operation      | Time Complexity |
+|-----------------|-----------------|
+| Dijkstra's Algorithm|O((V + E) log V)|
+
+
+DESCRIPTION
+
+Dijkstra's algorithm is a method for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. It operates by maintaining a set of tentative distances to each node, initially all set to infinity, and gradually updating these distances as it explores the graph. The algorithm visits each node in the graph exactly once, determining the shortest path to that node from the source node, and then updating the tentative distances to adjacent nodes if a shorter path is found. This process continues until all nodes have been visited and the shortest path to each node from the source has been determined.
+
+
+
+
+
---
+++ src/algoritms/searching/dijkstra/index.js
@@ -0,0 +1,54 @@
+const graph = {};
+
+graph.a = { b: 2, c: 1 }
+graph.b = { f: 7 }
+graph.c = { d: 5, e: 2 }
+graph.d = { f:  2}
+graph.e = { f: 1 }
+graph.f = { g: 1 }
+graph.g = {}
+
+const findNodeLowestCost = (costs, processed) => {
+    let lowestCost = Infinity;
+    let lowestNode;
+
+    Object.keys(costs).forEach(node => {
+        let cost = costs[node];
+        if (cost < lowestCost && !processed.includes(node)) {
+            lowestCost = cost;
+            lowestNode = node;
+        }
+    })
+
+    return lowestNode;
+}
+
+const shortPath = (qraph, start, end) => {
+    const costs = {}; // shortest ways
+    const processed = []; 
+    let neighbors = {};
+
+    Object.keys(qraph).forEach(node => {
+        if (node !== start) {
+            let value = qraph[start][node];
+            costs[node] = value || Infinity
+        }
+    })
+    let node = findNodeLowestCost(costs, processed);
+
+    while (node) {
+        const cost = costs[node];
+        neighbors = qraph[node];
+        Object.keys(neighbors).forEach(neighbor => {
+            const newCost = cost + neighbors[neighbor];
+            if (newCost < costs[neighbor]) {
+                costs[neighbor] = newCost;
+            }
+        });
+        processed.push(node);
+        node = findNodeLowestCost(costs, processed);
+    }
+    return costs;
+}
+
+console.log(shortPath(graph, 'a', 'g'))
