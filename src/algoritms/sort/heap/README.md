1712c7b74bb22f3510c2cdfff9c253791aa2d3fc
Bohdan <bogdaneskor@gmail.com>
2024-03-14 21:04:42 +0200

    add heap

---
+++ src/algoritms/sort/heap/README.md
@@ -0,0 +1,10 @@
+## Heap Sort
+
+| Complexity         | Big O          |
+| ------------------ | -------------- |
+| Average Complexity | θ(n \* log(n)) |
+| Best Case          | Ω(n \* log(n)) |
+| Worst Case         | O(n \* log(n)) |
+| Space Complexity   | O(1)           |
+
+Heap sort is a comparison-based sorting algorithm that works by first building a heap from the given array and then repeatedly extracting the maximum (for a max-heap) or minimum (for a min-heap) element from the heap and placing it at the end of the array. This process effectively sorts the array in ascending order (for max-heap) or descending order (for min-heap).
---
+++ src/algoritms/sort/heap/index.ts
@@ -0,0 +1,47 @@
+function swap <T>(arr: T[], i: number, j: number) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function heapify<T>(
+	arr: T[],
+	from: number,
+	to: number,
+	comparator: (a: T, b: T) => number,
+) {
+	const left = 2 * from + 1;
+	const right = 2 * from + 2;
+	let max = from;
+
+	if (left < to && comparator(arr[left], arr[max]) > 0) {
+		max = left;
+	}
+
+	if (right < to && comparator(arr[right], arr[max]) > 0) {
+		max = right;
+	}
+
+	if (max !== from) {
+		swap(arr, from, max);
+		heapify(arr, max, to, comparator);
+	}
+}
+
+function buildHeap<T>(arr: T[], comparator: (a: T, b: T) => number) {
+	for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
+		heapify(arr, i, arr.length, comparator);
+	}
+	return arr;
+}
+
+function heapSort<T>(arr: T[], comparator: (a: T, b: T) => number) {
+	buildHeap(arr, comparator);
+
+	for (let i = arr.length - 1; i > 0; i--) {
+		swap(arr, 0, i);
+		heapify(arr, 0, i, comparator);
+	}
+
+	return arr;
+}
