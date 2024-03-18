aacf616bfa652af567ca1bf28d6a753cb58a1fad
Bohdan <bogdaneskor@gmail.com>
2024-03-18 10:01:25 +0200

    start SinglyLinkedList

---
+++ src/data-strucrure/singly-linked-list/index.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class SinglyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+        this.length++;
+        const node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        return this;
+    }
+
+    pop() {
+        if (!this.length) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+};
+
+const list = new SinglyLinkedList();
+list.push('hi');
+list.push('you');
