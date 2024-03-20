0ed5fb6ed2ba55c0b2462185a5762657f1f52e04
Bohdan <bogdaneskor@gmail.com>
2024-03-20 20:44:42 +0200

    add new methods

--- src/data-strucrure/singly-linked-list/index.js
+++ src/data-strucrure/singly-linked-list/index.js
@@ -76,4 +76,53 @@ class SinglyLinkedList {
         }
         return current;
     }
+
+    set(index, val) {
+        const node = this.get(index);
+        if (node) {
+            node.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val);
+        if (index === 0) return !!this.unshift(val);
+        const newNode = new Node(val)
+        const prevNode = this.get(index - 1);
+        const prevNext = prevNode.next;
+        prevNode.next = newNode;
+        newNode.next = prevNext;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === this.length - 1) return this.pop();
+        if (index === 0) return this.shift();
+        const prevNode = this.get(index - 1);
+        const removedNode = prevNode.next;
+        prevNode.next = removedNode.next;
+        this.length--;
+        return removedNode;
+    }
+
+    reverse() {
+        const node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        let prev = null;
+        let next;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
 };
