fc0bb38498ff00eca1f36d9baca567438afa0518
Bohdan <bogdaneskor@gmail.com>
2024-03-20 10:03:56 +0200

    Add new methods to SingliLinkedList

--- src/data-strucrure/singly-linked-list/index.js
+++ src/data-strucrure/singly-linked-list/index.js
@@ -41,8 +41,39 @@ class SinglyLinkedList {
         }
         return current;
     }
-};
 
-const list = new SinglyLinkedList();
-list.push('hi');
-list.push('you');
+    shift() {
+        if (!this.head) return undefined;
+        this.length--;
+        const current = this.head;
+        this.head = current.next;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return current;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+            this.length++
+        }
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+};
