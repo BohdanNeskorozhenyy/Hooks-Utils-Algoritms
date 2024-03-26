15758f066cf6e9b853194221e6be195baf3cab39
Bohdan <bogdaneskor@gmail.com>
2024-03-26 09:28:40 +0200

    started DoublyLinkedList

---
+++ src/data-strucrure/list/doubly-linked-lists/index.js
@@ -0,0 +1,59 @@
+class _Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const node = new _Node(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return this
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        const removedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = removedNode.prev;
+            this.tail.next = null;
+            removedNode.prev = null;
+        }
+        this.length--;
+        return removedNode;
+    }
+
+    shift() {
+        if (this.length === 0) return undefined;
+        const oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+}
